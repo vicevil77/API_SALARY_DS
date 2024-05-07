6b5683f0d466478da6610fab0686d68b373c266b
vicevil77 <120662253+vicevil77@users.noreply.github.com>
2024-05-07 11:00:39 +0200

    1

--- app_model.py
+++ app_model.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
 import subprocess
+import logging
 
 #EJEMPLO PARA LA PRUEBA:
 #ejemplo en clase :  /api/v1/predict?work_year=5&experience_level=Senior&employment_type=Full-time&job_title=Software%20Engineer&salary_currency=USD&salary_in_usd=50000&employee_residence=USA&remote_ratio=0.5&company_location=USA&company_size=100-500
